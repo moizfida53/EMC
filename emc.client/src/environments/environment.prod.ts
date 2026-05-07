d4ed15615b8d4ca5ae458b568141147d1cfc8e1e
moizfida <moiztahafida@gmail.com>
2026-05-07 17:11:04 +0300

    environment url changed

--- emc.client/src/environments/environment.prod.ts
+++ emc.client/src/environments/environment.prod.ts
@@ -16,7 +16,7 @@ export const environment = {
     clientId:    '8dc2a917-6763-4edd-af51-8448dbea3af3',
     tenantId:    'd7be1e00-bf67-40de-a606-1a75a207d413',
     // Replace with your Vercel URL after the first deploy and re-deploy.
-    redirectUri: 'https://YOUR-PROJECT.vercel.app',
+    redirectUri: 'https://emc-azure.vercel.app',
     authority:   'https://login.microsoftonline.com/d7be1e00-bf67-40de-a606-1a75a207d413',
     scopes:      ['api://c2fffef8-7833-4f64-8a24-b3117374bf8e/access_as_user'],
   },
